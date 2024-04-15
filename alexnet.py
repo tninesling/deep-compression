8ed1ba497d5c3fef36b9a7ebafe70385f8c8252f
Taylor Ninesling <taylor.ninesling@gmail.com>
2024-04-14 21:21:53 -0500

    Refactor model evaluation

--- alexnet.py
+++ alexnet.py
@@ -1,102 +1,10 @@
-import os
+import yaml
 import time
 import torch
-import torch.nn.functional as F
 import torchvision
-import xml.etree.ElementTree as Xml
-from dotenv import load_dotenv
-from PIL import Image
-from torch.utils.data import DataLoader, Dataset
-
-load_dotenv()
-imagenet_annotations_dir = os.getenv("IMAGENET_ANNOTATIONS_DIR")
-imagenet_data_dir = os.getenv("IMAGENET_DATA_DIR")
-imagenet_synset_file = os.getenv("IMAGENET_SYNSET_FILE")
-
-BATCH_SIZE = 32
-NUM_PREDICTIONS = 5
-PADDING_VALUE = -1
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-num_workers = os.cpu_count()
-print(f"device: {device}, batch size: {BATCH_SIZE}, workers: {num_workers}")
-
-synsets = []
-with open(imagenet_synset_file) as synset_file:
-    for line in synset_file.readlines():
-        [synset_id, _] = line.split(" ", 1)
-        synsets.append(synset_id)
-
-
-class Annotation:
-    def __init__(self, file_path):
-        self.labels = set()
-
-        xml = Xml.parse(file_path).getroot()
-        filename = xml.find("filename").text
-        if not file_path.endswith(f"{filename}.xml"):
-            raise Exception("File name mismatch with annotation")
-
-        for object in xml.iter("object"):
-            name = object.find("name").text
-            if name is not None:
-                self.labels.add(name)
-
-            if len(self.labels) == NUM_PREDICTIONS:
-                break
-
-    def synset_idx_tensor(self):
-        labels = torch.tensor([synsets.index(label) for label in self.labels])
-        # Pad the tensor so all label tensors are the same size as the number of predictions.
-        # The fact that the match the prediction tensor size is somewhat arbitrary, but all
-        # label tensors must be the same size, regardless of what that size is.
-        return F.pad(
-            labels,
-            (0, NUM_PREDICTIONS - len(labels)),
-            mode="constant",
-            value=PADDING_VALUE,
-        )
-
-
-class KaggleImageNetDataset(Dataset):
-    def __init__(self, annotations_path, data_path):
-        self.annotations_path = annotations_path
-        self.data_path = data_path
-        self.img_names = os.listdir(data_path)
-        self.img_names.sort()
-        self.transforms = torchvision.models.AlexNet_Weights.IMAGENET1K_V1.transforms()
-
-    def __len__(self):
-        return len(self.img_names)
-
-    def __getitem__(self, i):
-        img_path = os.path.join(self.data_path, self.img_names[i])
-        img = Image.open(img_path).convert("RGB")
-        annotation_path = os.path.join(
-            self.annotations_path, self.img_names[i].replace("JPEG", "xml")
-        )
-        annotation = Annotation(annotation_path)
-
-        return self.transforms(img), annotation.synset_idx_tensor()
-
-
-class AlexNet:
-    def __init__(self):
-        self.model = torchvision.models.alexnet(
-            weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1
-        )
-
-    def eval(self):
-        self.model.eval()
-
-    def predict(self, images):
-        outputs = self.model(images)
-        probabilities = torch.nn.functional.softmax(outputs, dim=1)
-        _, top_idx = torch.topk(probabilities, NUM_PREDICTIONS)
-        return top_idx
-
-    def to(self, device):
-        self.model.to(device)
+from config import Config
+from kaggle_imagenet import KaggleImageNetDataset
+from torch.utils.data import DataLoader
 
 
 def nonempty_intersection(tensor1, tensor2):
@@ -104,41 +12,79 @@ def nonempty_intersection(tensor1, tensor2):
     return intersection_mask.any()
 
 
+class AlexNet:
+    def __init__(self, config: Config):
+        self.config = config
+        self.model = torchvision.models.alexnet(
+            weights=torchvision.models.AlexNet_Weights.IMAGENET1K_V1
+        )
+        self.model.eval()
+        self.model.to(config.runtime.device)
+
+    def predict(self, images):
+        outputs = self.model(images)
+        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+        _, top_idx = torch.topk(probabilities, self.config.runtime.num_predictions)
+        return top_idx
+
+
+class Evaluator:
+    def __init__(self, config: Config):
+        self.config = config
+        self.correct_predictions = 0
+        self.total_images = 0
+
+    def evaluate(self, dataloader: DataLoader, alexnet: AlexNet):
+        print("Evaluating model...")
+        self.correct_predictions = 0
+        self.total_images = 0
+        with torch.no_grad():
+            for image_batch, label_batch in dataloader:
+                image_batch = image_batch.to(self.config.runtime.device)
+                label_batch = label_batch.to(self.config.runtime.device)
+                prediction_batch = alexnet.predict(image_batch)
+
+                for predictions, labels in zip(prediction_batch, label_batch):
+                    self.total_images += 1
+                    if nonempty_intersection(predictions, labels):
+                        self.correct_predictions += 1
+
+                    if self.should_report_progress():
+                        self.report_progress(dataloader)
+
+    def should_report_progress(self):
+        ten_percent = len(dataloader.dataset) / 10
+        return self.total_images % ten_percent == 0
+
+    def report_progress(self, dataloader: DataLoader):
+        progress_percent = self.total_images / len(dataloader.dataset) * 100
+        accuracy = self.correct_predictions / self.total_images * 100
+        print(
+            f"{progress_percent:.1f}% complete, {accuracy:.1f}% accurate (top-{self.config.runtime.num_predictions})"
+        )
+
+
+class TimedEvaluator(Evaluator):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def evaluate(self, dataloader: DataLoader, alexnet: AlexNet):
+        self.start_time = time.time()
+        super().evaluate(dataloader, alexnet)
+        print(f"Executed in {time.time() - self.start_time:.1f}s")
+
+
 if __name__ == "__main__":
-    imagenet = KaggleImageNetDataset(
-        imagenet_annotations_dir,
-        imagenet_data_dir,
+    config = Config()
+    print(yaml.dump(config))
+
+    imagenet = KaggleImageNetDataset(config)
+    dataloader = DataLoader(
+        imagenet,
+        batch_size=config.runtime.batch_size,
+        num_workers=config.runtime.num_workers,
     )
-    dataloader = DataLoader(imagenet, batch_size=32, num_workers=num_workers)
-    alexnet = AlexNet()
+    alexnet = AlexNet(config)
 
-    print("Evaluating model...")
-    alexnet.eval()
-    alexnet.to(device)
-
-    correct = 0
-    total = 0
-    last_checkpoint = 0
-    start_time = time.time()
-    with torch.no_grad():
-        checkpoint_start_time = time.time()
-        for image_batch, label_batch in dataloader:
-            image_batch = image_batch.to(device)
-            label_batch = label_batch.to(device)
-            prediction_batch = alexnet.predict(image_batch)
-
-            for predictions, labels in zip(prediction_batch, label_batch):
-                total += 1
-                if nonempty_intersection(predictions, labels):
-                    correct += 1
-
-            if total - last_checkpoint > 1000:
-                print(
-                    f"{total / len(imagenet) * 100:.1f}% complete. Time since last checkpoint: {time.time() - checkpoint_start_time:.1f}s"
-                )
-                last_checkpoint = total
-                checkpoint_start_time = time.time()
-
-    accuracy = correct / total * 100
-    print(f"Top-{NUM_PREDICTIONS} accuracy: {accuracy:.1f}% ({correct} / {total})")
-    print(f"Executed in {time.time() - start_time:.1f}s")
+    evaluator = TimedEvaluator(config)
+    evaluator.evaluate(dataloader, alexnet)
---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+import torch
+from dotenv import load_dotenv
+
+
+class Config:
+    def __init__(self):
+        load_dotenv()
+        self.imagenet = ImageNetConfig()
+        self.runtime = RuntimeConfig()
+
+
+class ImageNetConfig:
+    def __init__(self):
+        self.annotations_dir = os.getenv("IMAGENET_ANNOTATIONS_DIR")
+        self.data_dir = os.getenv("IMAGENET_DATA_DIR")
+        self.synset_file = os.getenv("IMAGENET_SYNSET_FILE")
+
+
+class RuntimeConfig:
+    def __init__(self):
+        self.batch_size = int(os.getenv("BATCH_SIZE"))
+        self.device = torch.device(
+            os.getenv("DEVICE") or "cuda" if torch.cuda.is_available() else "cpu"
+        )
+        self.num_predictions = int(os.getenv("NUM_PREDICTIONS"))
+        self.num_workers = int(os.getenv("NUM_WORKERS") or os.cpu_count())
