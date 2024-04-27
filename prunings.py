1ff3c2dd501ef6e699d6faa9d34a193f5d4c435b
Anwesa Goswami <anwesagoswami@Anwesas-MacBook-Air.local>
2024-04-27 16:24:01 -0500

    added pruning methods

--- prunings.py
+++ prunings.py
@@ -1,2 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.utils.prune as prune
+
+prune_amount = 0.2
+
 def no_prune(model):
     return model
+    
+def l1_unstructured_prune(model):
+  for name, module in model.named_modules():
+    if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
+      prune.l1_unstructured(module, name="weight", amount=prune_amount)
+      prune.remove(module, 'weight')
+  return model
+  
+def random_unstructured_prune(model):
+  for name, module in model.named_modules():
+    if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
+      prune.random_unstructured(module, name="weight", amount=prune_amount)
+      prune.remove(module, 'weight')
+  return model
+  
+def global_unstructured_prune(model):
+  parameters_to_prune = []
+  for name, module in model.named_modules():
+    if isinstance(module, nn.Linear):
+        parameters_to_prune.append((getattr(model, name), 'weight'))
+  prune.global_unstructured(
+      parameters_to_prune,
+      pruning_method=prune.L1Unstructured,
+      amount=prune_amount
+  )
+  for name, module in model.named_modules():
+    if isinstance(module, nn.Linear):
+        prune.remove(module, 'weight')
+  return model
